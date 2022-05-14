7af0c49a8f9f48fcd51e05f13e663d88aa2252f8
Nicolas Schlecker <nicolasschlecker@gmail.com>
2022-05-15 00:51:31 +0200

    feat: add CustomAppBar

--- frontend/src/presentation/Root.tsx
+++ frontend/src/presentation/Root.tsx
@@ -1,18 +1,22 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
 import theme from './theme';
 import './i18n';
+import keycloak from '../Keycloak';
 
 export const Root: React.FC = () => {
 	return (
-		<BrowserRouter>
-			<ThemeProvider theme={theme}>
-				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-				<CssBaseline />
-				<Router />
-			</ThemeProvider>
-		</BrowserRouter>
+		<ReactKeycloakProvider authClient={keycloak}>
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+					<CssBaseline />
+					<Router />
+				</ThemeProvider>
+			</BrowserRouter>
+		</ReactKeycloakProvider>
 	);
 };
--- frontend/src/presentation/pages/HomePage.tsx
+++ frontend/src/presentation/pages/HomePage.tsx
@@ -1,8 +1,9 @@
 import Typography from '@mui/material/Typography/Typography';
 import React from 'react';
+import CustomAppBar from '../components/CustomAppBar';
 
 const HomePage: React.FC = () => {
-	return <Typography variant='h1'>Home</Typography>;
+	return <CustomAppBar />;
 };
 
 export default HomePage;
