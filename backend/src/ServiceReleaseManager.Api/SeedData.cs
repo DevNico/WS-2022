ba1449129a0b30f319d400170264cdee66878ebe
Nicolas Schlecker <nicolasschlecker@gmail.com>
2022-06-02 21:18:36 +0200

    remove unused code

--- backend/src/ServiceReleaseManager.Api/SeedData.cs
+++ backend/src/ServiceReleaseManager.Api/SeedData.cs
@@ -9,24 +9,6 @@ public static class SeedData
   public static readonly Organisation TestOrganisation1 =
     new("Test Organisation");
 
-  // public static readonly ToDoItem ToDoItem1 = new ToDoItem
-  // {
-  //   Title = "Get Sample Working", Description = "Try to get the sample to build."
-  // };
-  //
-  // public static readonly ToDoItem ToDoItem2 = new ToDoItem
-  // {
-  //   Title = "Review Solution",
-  //   Description =
-  //     "Review the different Organisations in the solution and how they relate to one another."
-  // };
-  //
-  // public static readonly ToDoItem ToDoItem3 = new ToDoItem
-  // {
-  //   Title = "Run and Review Tests",
-  //   Description = "Make sure all the tests run and review what they are doing."
-  // };
-
   public static void Initialize(IServiceProvider serviceProvider)
   {
     using var dbContext = new AppDbContext(
