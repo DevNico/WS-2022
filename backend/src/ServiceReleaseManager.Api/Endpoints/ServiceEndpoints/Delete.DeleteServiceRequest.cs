97816720532f39dd42a02d451a523fd6057824fb
MseitenDE <martin@mseiten.de>
2022-06-19 11:50:13 +0200

    Implemented suggestions

--- backend/src/ServiceReleaseManager.Api/Endpoints/ServiceEndpoints/Delete.DeleteServiceRequest.cs
+++ backend/src/ServiceReleaseManager.Api/Endpoints/ServiceEndpoints/Delete.DeleteServiceRequest.cs
@@ -4,6 +4,7 @@ namespace ServiceReleaseManager.Api.Endpoints.ServiceEndpoints;
 
 public class DeleteServiceRequest
 {
-  public const string Route = "{serviceId:int}";
+  public const string Route = "{ServiceId:int}";
+  
   [Required] public int ServiceId { get; set; }
 }
--- backend/src/ServiceReleaseManager.Api/Endpoints/ServiceEndpoints/GetById.GetServiceByIdRequest.cs
+++ backend/src/ServiceReleaseManager.Api/Endpoints/ServiceEndpoints/GetById.GetServiceByIdRequest.cs
@@ -4,6 +4,7 @@ namespace ServiceReleaseManager.Api.Endpoints.ServiceEndpoints;
 
 public class GetServiceByIdRequest
 {
-  public const string Route = "{serviceId:int}";
+  public const string Route = "{ServiceId:int}";
+  
   [Required] public int ServiceId { get; set; }
 }
