d5eba622251ddfee79ef7d5b671615e46fe0a82c
Nicolas Schlecker <nicolasschlecker@gmail.com>
2022-06-26 20:11:59 +0200

    work on release ui

--- frontend/src/presentation/pages/service/ReleasesPage.tsx
+++ frontend/src/presentation/pages/service/ReleasesPage.tsx
@@ -1,7 +1,33 @@
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import AddIcon from '@mui/icons-material/Add';
 
 const ReleasesPage: React.FC = () => {
-	return <h1>Releases</h1>;
+	const { t } = useTranslation();
+
+	return (
+		<>
+			<Stack
+				direction={['column', 'column', 'row']}
+				justifyContent='space-between'
+				mb={2}
+			>
+				<Typography variant='h4' component='h2'>
+					{t('release.list.title')}
+				</Typography>
+				<Button
+					// onClick={handleOpenCreateDialog}
+					startIcon={<AddIcon />}
+					sx={{ width: 'max-content' }}
+				>
+					{t('release.list.create')}
+				</Button>
+			</Stack>
+		</>
+	);
 };
 
 export default ReleasesPage;
